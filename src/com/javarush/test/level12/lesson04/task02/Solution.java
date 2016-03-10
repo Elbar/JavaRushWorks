c04d4ef10ff97488636e196afd4aace0f219c5c9
Vista <vista-e@mail.ru>
2016-03-10 20:20:06 +0600

    Added 12'th level

---
+++ src/com/javarush/test/level12/lesson04/task02/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level12.lesson04.task02;
+
+/* print(int) и print(Integer)
+Написать два метода: print(int) и print(Integer).
+Написать такой код в методе main, чтобы вызвались они оба.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public int print(int n)
+    {
+        return n;
+    }
+
+    public Integer print(Integer n)
+    {
+        return n;
+    }
+
+    //Напишите тут ваши методы
+}
---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+    public int print(int n, int m)
+    {
+        return n;
+    }
+
+    public String print(String s)
+    {
+        return s;
+    }
+    public int print(int n)
+    {
+        return n;
+    }
+
+    public String print(String s, String string)
+    {
+
+        return s;
+    }
+
+    public int print(int n, short sh)
+    {
+
+        return n;
+    }
+
+
+
+    //Напишите тут ваши методы
+}
